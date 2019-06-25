2e2b6781357302412d91560943a75ef1eaf52121
vladhmelevskiy1234765 <vladhmelevskiy1234765@gmail.com>
2019-06-25 13:06:59 +0300

    exception to det=0; to n!=p.Lenght; to words, don`t num

--- KramerMetod/KramerMetod/Program.cs
+++ KramerMetod/KramerMetod/Program.cs
@@ -8,26 +8,34 @@ namespace KramerMetod
     {
         static void Main(string[] args)
         {
-            (int[,] matrix, int[,] ansverMatrix) = ReadString();
+            Console.WriteLine("Kramer method");
+            (int[,] matrix, int[,] ansverMatrix) = ReturnTwoMatrix();
             var determinantBasicMatrix = Determinant(matrix);
-            Console.WriteLine($"Det = {determinantBasicMatrix}");
-            int[] oldMatrix = new int[matrix.GetLength(0)];
-            var allDeterminantDivideRoBasicMatrix = 0d;
-            for (int number = 0; number < matrix.GetLength(0); number++)
+            Console.WriteLine($"Determinant = {determinantBasicMatrix}");
+            if (determinantBasicMatrix == 0)
             {
-                for (int i = 0; i < matrix.GetLength(0); i++)
-                {
-                    oldMatrix[i] = matrix[i, number];
-                    matrix[i, number] = ansverMatrix[i, 0];
-                }
-                allDeterminantDivideRoBasicMatrix = (double)Determinant(matrix) / (double)determinantBasicMatrix;
-                Console.WriteLine($"x{number+1} = {allDeterminantDivideRoBasicMatrix}");
-                for (int i = 0; i < matrix.GetLength(0); i++)
-                {
-                    matrix[i, number] = oldMatrix[i];
-                }
+                Console.ReadKey();
             }
-            Console.ReadKey();
+            else
+            {
+                int[] oldMatrix = new int[matrix.GetLength(0)];
+                var allDeterminantDivideRoBasicMatrix = 0d;
+                for (int number = 0; number < matrix.GetLength(0); number++)
+                {
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        oldMatrix[i] = matrix[i, number];
+                        matrix[i, number] = ansverMatrix[i, 0];
+                    }
+                    allDeterminantDivideRoBasicMatrix = (double)Determinant(matrix) / (double)determinantBasicMatrix;
+                    Console.WriteLine($"x{number + 1} = {allDeterminantDivideRoBasicMatrix}");
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        matrix[i, number] = oldMatrix[i];
+                    }
+                }
+                Console.ReadKey();
+            }
         }
         static int Determinant(int[,] matrix)
         {
@@ -63,102 +71,71 @@ namespace KramerMetod
             }
             return smallMatrix;
         }
-        static void WriteMatrix(int[,] mat)
+        static (int[,], int[,]) ReturnTwoMatrix()
         {
-            Console.WriteLine();
-            for (int i = 0; i < mat.GetLength(0); i++)
-            {
-                for (int j = 0; j < mat.GetLength(1); j++)
-                {
-                    Console.Write($"{mat[i, j]}\t");
-                }
-                Console.WriteLine();
-            }
-        }
-        static int[,] ReadMatrix()
-        {
-            var (n, m) = ReadBorder();
-            int[,] matrix = new int[n, m];
-            Console.WriteLine("Write matrix");
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    matrix[i, j] = ReadIntUnit(' ');
-                }
-                Console.WriteLine();
-            }
-            return matrix;
-
-        }
-        static int ReadIntUnit(params char[] unitSymbol)
-        {
-            StringBuilder sb = new StringBuilder();
-            while (true)
-            {
-                var key = Console.ReadKey();
-                if (unitSymbol.Contains(key.KeyChar))
-                {
-                    return Int32.Parse(sb.ToString());
-                }
-                else
-                {
-                    sb.Append(key.KeyChar);
-                }
-            }
-        }
-        static (int n, int m) ReadBorder()
-        {
-            Console.WriteLine("Enter the number of equation");
-            Console.Write("Number of equation=");
-            var n = ReadInt();
-            Console.WriteLine("Enter the number of variables");
-            Console.Write("Number of variables=");
-            var m = ReadInt();
-            return (n, m);
-        }
-        static int ReadInt()
-        {
-            var key = Console.ReadKey();
-            if (key.Key == ConsoleKey.Escape)
-            {
-                throw new OperationCanceledException();
-            }
-            var line = Console.ReadLine();
-            var keyLine = key.KeyChar + line;
-            return Int32.Parse(keyLine);
-        }
-        static (int[,], int[,]) ReadString()
-        {
-            var (n, m) = ReadBorder();
-            int[,] matrixEquation = new int[n,m];
+            var n = ReadInt("number of equation");
+            int[,] matrixEquation = new int[n, n];
             int[,] ansverMatrix = new int[n,1];
-            var numEquation = 1;
-            while (true)
+            for (int i = 0; i < matrixEquation.GetLength(0); i++)
             {
-                string stringEquation = Read($"{numEquation} equation");
+                string stringEquation = ReadString($"{i + 1} equation without index x");
                 var printEquation = Split(stringEquation);
-                ansverMatrix[numEquation-1,0] = Int32.Parse(printEquation[printEquation.Length-1]);
-                for(int j = 0; j < printEquation.Length-1; j++)
+                    do
+                    {
+                        try
+                        {
+                            if (printEquation.Length == n + 1)
+                            {
+                                ansverMatrix[i, 0] = Int32.Parse(printEquation[printEquation.Length - 1]);
+                                for (int j = 0; j < printEquation.Length - 1; j++)
+                                {
+                                    matrixEquation[i, j] = Int32.Parse(printEquation[j]);
+                                }
+                            }
+                            break;
+                        }
+                        catch(FormatException ex)
+                        {
+                            Console.WriteLine($"Bed input {ex.Message}, try gain or click Escape");
+                            stringEquation = ReadString($"{i + 1} equation without index x");
+                            printEquation = Split(stringEquation);
+                        }
+                    } while (true);
+                
+                if (printEquation.Length != n + 1)
                 {
-                    matrixEquation[numEquation-1, j] = Int32.Parse(printEquation[j]);
-                }
-                numEquation++;
-                var key = Console.ReadKey();
-                if(key.Key == ConsoleKey.Tab)
-                {
-                    return (matrixEquation, ansverMatrix);
+                    Console.WriteLine("Bed input, try again");
+                    i--;
+                    continue;
                 }
             }
+            return (matrixEquation, ansverMatrix);
         }
-        static string[] Split(string split)
-        {
-            return split.Split(new[] { ' ', 'x', '=' }, StringSplitOptions.RemoveEmptyEntries);
-        }
-        static string Read(string read)
+        static int ReadInt(string  readString)
         {
             do
             {
+                try
+                {
+                    Console.WriteLine($"Enter {readString}");
+                    Console.Write($"{readString} = ");
+                    var key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Escape)
+                    {
+                        throw new OperationCanceledException();
+                    }
+                    var line = Console.ReadLine();
+                    var keyLine = key.KeyChar + line;
+                    return Int32.Parse(keyLine);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine($"Bed input {ex.Message}, try again or click Escape");
+                }
+            } while (true);
+        }
+        static string ReadString(string read)
+        {
                 Console.WriteLine($"Enter {read}");
                 var key = Console.ReadKey();
                 if (key.Key == ConsoleKey.Escape)
@@ -168,7 +145,10 @@ namespace KramerMetod
                 var line = Console.ReadLine();
                 var keylin = $"{key.KeyChar}{line}";
                 return Convert.ToString(keylin);
-            } while (true);
+        }
+        static string[] Split(string split)
+        {
+            return split.Split(new[] { ' ', 'x', '=' }, StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
